833b01d39eb6859a5371a6688069410e3bd15b6c
Kapil Neela <neelakapil15@gmail.com>
2026-09-22 23:42:23 +0530

    Create 1_Two-Sum.py

---
+++ Practice/LeetCode/1_Two-Sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums, target):
+        list1 = []
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+
+                if nums[i] + nums[j] == target:
+                    list1.append(i)
+                    list1.append(j)
+                    return list1
